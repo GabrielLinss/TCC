22c2d890c7a73edeedb295a4062509d05f8238f5
GabrielLinss <glins731@gmail.com>
2019-05-02 01:38:50 -0300

    add the basic structure and dependencies for use redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
+import {Jumbotron, Button} from 'react-bootstrap';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>TCC</h1>
+      <div>
+        <Jumbotron>
+          <h1>Olá, bem vindo</h1>
+          <p>Aqui você tem acesso aos serviços da receita federal</p>
+          <p>
+            <Button variant="primary">Acessar</Button>
+          </p>
+        </Jumbotron>
       </div>
     );
   }
