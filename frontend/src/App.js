2bfd193af38629f551f77e24eeffa6d4936e4bb4
GabrielLinss <glins731@gmail.com>
2019-05-10 10:51:35 -0300

    [DEV 01] add initial navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 
-import {Jumbotron, Button} from 'react-bootstrap';
+import {Jumbotron, Button, Container, Row, Col} from 'react-bootstrap';
+
+import './App.scss';
+
+import Navbar from './components/navbar';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Navbar/>
+        
         <Jumbotron>
-          <h1>Olá, bem vindo</h1>
-          <p>Aqui você tem acesso aos serviços da receita federal</p>
+          <h1>Olá, seja bem vindo(a) ao sistema</h1>
+          <p>Aqui você tem acesso aos serviços de alocação de salas do campus</p>
           <p>
             <Button variant="primary">Acessar</Button>
           </p>
         </Jumbotron>
+        
+        <Container>
+          <Row>
+            <Col></Col>
+          </Row>
+        </Container>
       </div>
     );
   }
